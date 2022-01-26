eb1a445637b2e28c3d64cb55d22ad9dfaa5edff3
Aldiansyah Fandevtian <fandevtian@gmail.com>
2022-01-26 21:07:46 +0700

    add reac-router-dom and basic route

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ imageUrl, title, size }) => (
-  <div className={`${size} menu-item`}>
-    <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }}></div>
+const MenuItem = ({ imageUrl, title, size, history, match, linkUrl }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
+    <div
+      className="background-image"
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    ></div>
     <div className="content">
       <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">SHOP NOW</span>
@@ -11,4 +18,4 @@ const MenuItem = ({ imageUrl, title, size }) => (
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
